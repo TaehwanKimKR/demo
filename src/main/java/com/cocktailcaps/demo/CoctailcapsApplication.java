7bd0286293907d5098e26e833fd41a83752083ac
김태환 <appleit1245@naver.com>
2023-11-07 23:13:18 +0900

    ver 1.0

---
+++ src/main/java/com/cocktailcaps/demo/CoctailcapsApplication.java
@@ -0,0 +1,13 @@
+package com.cocktailcaps.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoctailcapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoctailcapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cocktailcaps/demo/CoctailcapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocktailcaps.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoctailcapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
