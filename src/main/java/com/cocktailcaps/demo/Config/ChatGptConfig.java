a8f2fef67c6549083d2e6fea2178a286c1852c1c
김태환 <appleit1245@naver.com>
2023-11-21 02:12:49 +0900

    ver 1.1 CORS comp

--- src/main/java/com/cocktailcaps/demo/Config/ChatGptConfig.java
+++ src/main/java/com/cocktailcaps/demo/Config/ChatGptConfig.java
@@ -3,7 +3,7 @@ package com.cocktailcaps.demo.Config;
 public class ChatGptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "api입력";
+    public static final String API_KEY = "apikey";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 512;
     public static final Double TEMPERATURE = 0.0;
